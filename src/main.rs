63e38cd0ce2d016f14f24e2ebf83703795e4894e
Marshall Bowers <elliott.codes@gmail.com>
2022-07-26 23:24:16 -0400

    Write Last.fm tracks to file

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,61 @@
 mod lastfm;
 
+use std::env;
+
+use chrono::{DateTime, Utc};
+use dotenv::dotenv;
+use serde::Serialize;
+use tokio::{fs::File, io::AsyncWriteExt};
+
 use lastfm::GetRecentTracksResponse;
+
+#[derive(Debug, Serialize)]
+struct Track {
+    pub name: String,
+    pub artist: String,
+    pub album: String,
+    pub listened_at: DateTime<Utc>,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const LASTFM_USER: &'static str = "";
-    const LASTFM_API_KEY: &'static str = "";
+    dotenv().ok();
 
-    let url = format!("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={api_key}&format=json", user = LASTFM_USER, api_key = LASTFM_API_KEY);
+    let lastfm_user = env::var("LASTFM_USER")?;
+    let lastfm_api_key = env::var("LASTFM_API_KEY")?;
+
+    let url = format!("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key={api_key}&format=json", user = lastfm_user, api_key = lastfm_api_key);
     let response = reqwest::get(url)
         .await?
         .json::<GetRecentTracksResponse>()
         .await?;
 
+    let mut formatted_tracks = Vec::new();
+
     for track in response.recent_tracks.track {
-        let x = dbg!(track);
+        let track = Track {
+            name: track.name,
+            artist: track.artist.name,
+            album: track.album.name,
+            listened_at: track.date.timestamp,
+        };
+
+        formatted_tracks.push(track);
     }
+
+    #[derive(Debug, Serialize)]
+    struct Output {
+        tracks: Vec<Track>,
+    }
+
+    let mut file = File::create("2022.toml").await?;
+    file.write_all(
+        toml::to_string_pretty(&Output {
+            tracks: formatted_tracks,
+        })?
+        .as_bytes(),
+    )
+    .await?;
 
     Ok(())
 }
